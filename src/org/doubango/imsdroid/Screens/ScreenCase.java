0377133a330c385ab1d062df385cc09b38cb346f
Francisco Alonso <francisco.a.alonso.s@gmail.com>
2014-06-03 19:33:23 -0430

    casos

--- src/org/doubango/imsdroid/Screens/ScreenCase.java
+++ src/org/doubango/imsdroid/Screens/ScreenCase.java
@@ -4,8 +4,15 @@ import org.doubango.imsdroid.R;
 import org.doubango.imsdroid.Screens.BaseScreen.SCREEN_TYPE;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ScreenCase extends BaseScreen{
+	
+	private Button saveButton;
+	private Button opinionButton;
+	private Button cancelButton;
 	
 	private static String TAG = ScreenPresence.class.getCanonicalName();
 	
@@ -17,6 +24,38 @@ public class ScreenCase extends BaseScreen{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_case);
+        
+        saveButton = (Button)findViewById(R.id.saveCase);
+        opinionButton = (Button)findViewById(R.id.askOpinion);
+        cancelButton = (Button)findViewById(R.id.cancelEditCase);
+        
+        saveButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        opinionButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        cancelButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
 	}
 	
 	@Override
